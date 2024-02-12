4f3e8fccf96359f7fbcb56471b14d0a77c62d99a
Naveen-3354 <welbe1709@gmail.com>
2024-02-12 08:08:15 +0530

    Fiirst

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import "./Login.scss";
+import VisibilityRoundedIcon from "@mui/icons-material/VisibilityRounded";
+import VisibilityOffRoundedIcon from "@mui/icons-material/VisibilityOffRounded";
+
+const Login = () => {
+  const [passwordHide, setPasswordHide] = useState(false);
+
+  const handleVisibility = () => {
+    setPasswordHide(!passwordHide);
+  };
+  return (
+    <div className="login">
+      <div className="right">
+        <div className="logo">
+          <h2>EASY</h2>
+          <h2>FARM</h2>
+        </div>
+        <div className="title">
+          <h3>Create a account...</h3>
+        </div>
+        <div className="inputs">
+          <div className="input">
+            <input type="text" id="userName" placeholder="Email or Number" />
+          </div>
+          <div className="input password">
+            <input
+              type={passwordHide ? "text" : "password"}
+              id="password"
+              placeholder="Password"
+            />
+            <div className="visibility" onClick={handleVisibility}>
+              {passwordHide ? (
+                <VisibilityRoundedIcon className="eyeIcon" />
+              ) : (
+                <VisibilityOffRoundedIcon className="eyeIcon" />
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="forgot">
+          <p>Forget password?</p>
+        </div>
+        <div className="buttons">
+          <button className="signIn">Sign In </button>
+          <div className="or">
+            <div className="one"></div>
+            <div className="two"><p>Or</p></div>
+            <div className="one"></div>
+          </div>
+          <button className="signup">Sign Up </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
