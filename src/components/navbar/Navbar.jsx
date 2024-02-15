0192462b5ac12b2e5de8c1617d3e8e042c67087c
Naveen-3354 <welbe1709@gmail.com>
2024-02-15 19:47:02 +0530

    Navbar update

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,51 @@
+import "./Navbar.scss";
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+import ShoppingBagRoundedIcon from "@mui/icons-material/ShoppingBagRounded";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+import GridViewRoundedIcon from "@mui/icons-material/GridViewRounded";
+import NotificationsRoundedIcon from "@mui/icons-material/NotificationsRounded";
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+// import Switch from "../switch/Switch";
+import Theme from "../themes/Theme";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <h1>LOGO</h1>
+      </div>
+      <div className="search">
+        <input type="text" placeholder="Search...." />
+        <SearchRoundedIcon className="searchIcon" />
+      </div>
+      <div className="links">
+        <div className="home link">
+          <h3>Home</h3>
+        </div>
+        <div className="category link">
+          <GridViewRoundedIcon className="link-icon"/>
+          <h3>Category</h3>
+        </div>
+        <div className="bag link">
+          <ShoppingBagRoundedIcon className="link-icon"/>
+          <h3>Bag</h3>
+        </div>
+        <div className="order link">
+          <ShoppingCartRoundedIcon className="link-icon"/>
+          <h3>Orders</h3>
+        </div>
+        <div className="notification link">
+          <NotificationsRoundedIcon className="link-icon big"/>
+          {/* <h3>Notification</h3> */}
+        </div>
+        <div className="theme"><Theme/></div>
+        <div className="profile link">
+          <AccountCircleRoundedIcon className="link-icon big"/>
+          <h3>Profile</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../../components/navbar/Navbar"
+import "./Home.scss"
+
+const Home = () => {
+  return (
+    <div className="Home">
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Home
